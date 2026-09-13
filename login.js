587b6bed971a91fc65c4a6d94186508806a8de88
pedroparra1-debug <pedro.parra1@mail.udp.cl>
2026-09-13 01:24:17 -0300

    login para version celular listo, mejor diseño y con ambos botones

--- login.js
+++ login.js
@@ -1,4 +1,7 @@
 const form = document.getElementById('loginform');
 form.addEventListener('submit', function(event) {
 event.preventDefault();
+const clickedButton = event.submitter;
+const role = clickedButton.getAttribute('data-role') || 'encargado';
+localStorage.setItem('userrole', role);
 window.location.href='main.html';})
